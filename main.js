535b31be6c8a1e13c09e1ec05ad4ef1345d42aff
zxy <1378745457@qq.com>
2022-08-10 11:07:08 +0800

    init project

--- main.js
+++ main.js
@@ -2,6 +2,22 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入网络请求的包 ($http 对象)
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http // 挂载到顶级对象uni上
+// 设置请求的根路径
+$http.baseUrl = 'https://autumnfish.cn/wx'
+
+// 请求拦截器 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+      title: '数据加载中...', // 展示登录效果
+    })
+}
+// 响应拦截器 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()  // 隐藏登录效果
+}
 
 Vue.config.productionTip = false
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,9 +8,31 @@
   export default {
     data() {
       return {
-        
+        // 1. 轮播图的数据列表，默认为空数组
+        swiperList: [],
       };
-    }
+    },
+    onLoad() {
+        // 2. 在 onLoad 生命周期函数中(小程序页面刚加载的时候)，调用获取轮播图数据的方法
+        this.getSwiperList()
+      },
+      methods: {
+          // 3. 获取轮播图数据的方法
+          async getSwiperList() {
+            // 3.1 发起请求
+            const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+            // 3.2 请求失败
+            if (res.meta.status !== 200) {
+              return uni.showToast({
+                title: '数据请求失败！',
+                duration: 1500,
+                icon: 'none',
+              })
+            }
+            // 3.3 请求成功，为 data 中的数据赋值
+            this.swiperList = res.message
+          },
+        },
   }
 </script>
 
